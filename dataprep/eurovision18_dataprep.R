f867aa1c193978cd4cb31721d706ba7e4b697040
Maarten <maarten.lambrechts@datylon.com>
2018-05-01 14:57:27 +0200

    Gitignore dataprep folder

--- dataprep/eurovision18_dataprep.R
+++ dataprep/eurovision18_dataprep.R
@@ -6,7 +6,7 @@ library(googlesheets)
 library(countrycode)
 library(readxl)
 
-eurovision <- gs_title("Eurovision_TEST_result")
+eurovision <- gs_title("Eurovision_FINAL_RESULT")
 sf1 <- gs_read(eurovision, "SF1 Clean", range = cell_cols(1:25))
 sf2 <- gs_read(eurovision, "SF2 Clean", range = cell_cols(1:24))
 final <- gs_read(eurovision, "Simulate Final Clean", range = cell_cols(1:46))
@@ -17,7 +17,7 @@ votes <- gather(final, key = "from", value = "points", 4:46) %>%
   mutate(Country = countrycode(Country, 'country.name', 'iso3c'), from = countrycode(from, 'country.name', 'iso3c'))
 colnames(votes) <- c("to", "from", "points")
 
-write.csv(votes, "../data/votes_2018-04-18.csv", row.names = FALSE)
+write.csv(votes, "../data/votes_2018-05-01.csv", row.names = FALSE)
 
 ##Ranking
 ranking <- group_by(votes, to) %>%
@@ -27,7 +27,7 @@ ranking <- group_by(votes, to) %>%
   select(to)
 colnames(ranking) <- c("Country")
 
-write.csv(ranking, "../data/overallranking_2018-04-18.csv", row.names = FALSE)
+write.csv(ranking, "../data/overallranking_2018-05-01.csv", row.names = FALSE)
 
 ## Qualification
 participants <- read_excel("participants.xlsx")
@@ -47,5 +47,5 @@ participants[is.na(participants)] <- FALSE
 ##AFTER QUALIFICATION
 ##JOIN LIST WITH REAL QUALIFIERS, ADD TO COLUMN qualifiedreal
 
-write.csv(participants, "../data/qualification_2018-04-18.csv", row.names = FALSE)
+write.csv(participants, "../data/qualification_2018-05-01.csv", row.names = FALSE)
 
